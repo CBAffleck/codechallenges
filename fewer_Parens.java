d05d7e88b7414f56169ec75e462dec78b236efca
Campbell Affleck <cbaffleck@berkeley.edu>
2017-01-15 09:38:44 -0800

    Added description for program and some comments.

--- fewer_Parens.java
+++ fewer_Parens.java
@@ -2,11 +2,17 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+/*
+	fewer_Params takes a string input from the user and takes out any extra sets of parentheses. For example, it would reduce
+	((cat)) to (cat) and ((cat)()) to (cat).
+*/
+
 public class fewer_Parens {
 
 	public static String reduce(String input) {
 		StringBuilder str1 = new StringBuilder(input);
 		int i = 0;
+		// Gets rid of empty sets of parentheses, i.e. ab() >> ab
 		while (i < str1.length()) {
 			if (str1.charAt(i) == '(' && str1.charAt(i+1) == ')') {
 				str1.deleteCharAt(i + 1);
@@ -16,6 +22,7 @@ public class fewer_Parens {
 			}
 		}
 
+		// Gets rid of extra sets of parentheses, i.e. ((cat)) >> (cat)
 		int depth;
 		for (int j = 0; j < str1.length() - 1; j++) {
 			if (str1.charAt(j) == '(' && str1.charAt(j+1) == '(') {
@@ -38,6 +45,7 @@ public class fewer_Parens {
 	}
 
 	public static void main(String[] args) {
+		// Takes a string that's input by the user
 		System.out.println("Enter line of text: ");
 		Scanner in1 = new Scanner(System.in);
 		String usr_input = in1.nextLine();
